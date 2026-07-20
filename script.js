3ad04c2ba0e60ef46fdd0c1d5d739b594017b22d
Marcia MagaX <135170338+MarciaMagaX@users.noreply.github.com>
2026-07-20 15:29:21 -0300

    Primeira versão do projeto Muay Thai Academy

---
+++ script.js
@@ -0,0 +1,102 @@
+let golpes = [
+
+    {
+        nome: 'Low Kick',
+        imagemGolpe: 'imagens/lowkick.png',
+        defesa: 'Check',
+        imagemDefesa: 'imagens/check.png'
+    },
+
+    {
+        nome: 'Jab',
+        imagemGolpe: 'imagens/jab.png',
+        defesa: 'Bloqueio Alto',
+        imagemDefesa: 'imagens/bloqueioalto.png'
+    },
+
+    {
+        nome: 'Direto',
+        imagemGolpe: 'imagens/direto.png',
+        defesa: 'Esquiva',
+        imagemDefesa: 'imagens/esquiva.png'
+    },
+
+    {
+        nome: 'Teep',
+        imagemGolpe: 'imagens/teep.png',
+        defesa: 'Desvio Lateral',
+        imagemDefesa: 'imagens/desviolateral.png'
+    }
+
+]
+
+let defesas = [
+
+    'Check',
+    'Bloqueio Alto',
+    'Esquiva',
+    'Desvio Lateral'
+
+]
+
+let indiceGolpe
+
+function iniciar() {
+
+    indiceGolpe = Math.floor(Math.random() * golpes.length)
+
+    document.getElementById('txtGolpe').innerText =
+        golpes[indiceGolpe].nome
+
+    document.getElementById('imgGolpe').src =
+        golpes[indiceGolpe].imagemGolpe
+
+    document.getElementById('msgResultado').innerText = ''
+
+}
+
+function responder() {
+
+    let opcoes = document.getElementsByName('defesa')
+    let defesa
+
+    if (opcoes[0].checked) {
+        defesa = opcoes[0].value
+    } else if (opcoes[1].checked) {
+        defesa = opcoes[1].value
+    } else if (opcoes[2].checked) {
+        defesa = opcoes[2].value
+    } else if (opcoes[3].checked) {
+        defesa = opcoes[3].value
+    }
+
+    let golpe = document.getElementById('txtGolpe').innerText
+
+    if (golpe === 'Low Kick' && defesa === 'Check') {
+
+        document.getElementById('msgResultado').innerText =
+            '✅ Você acertou!'
+
+    } else if (golpe === 'Jab' && defesa === 'Bloqueio Alto') {
+
+        document.getElementById('msgResultado').innerText =
+            '✅ Você acertou!'
+
+    } else if (golpe === 'Direto' && defesa === 'Esquiva') {
+
+        document.getElementById('msgResultado').innerText =
+            '✅ Você acertou!'
+
+    } else if (golpe === 'Teep' && defesa === 'Desvio Lateral') {
+
+        document.getElementById('msgResultado').innerText =
+            '✅ Você acertou!'
+
+    } else {
+
+        document.getElementById('msgResultado').innerText =
+            '❌ Errou! Tente novamente.'
+
+    }
+
+}
